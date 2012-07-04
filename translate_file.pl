d6412a864fd8b46895fd7392bbdcd4e22c237172
Mark Fishel <fishel@cl.uzh.ch>
2012-07-04 11:00:50 +0200

    code clean-up

--- translate_file.pl
+++ translate_file.pl
@@ -7,66 +7,42 @@ use WWW::Curl::Form;
 use WWW::Curl::Easy;
 use utf8;
 
-##### Configuration #####
-our $config = confLoad("config.ini");
+#####################################################
 
-our $translHostList = confHash($config->{'translation host list'});
-our $recaseHostList = confHash($config->{'recasing host list'});
+# load configuration
+my $config = confLoad("config.ini");
 
-our $rawTextMode = ($config->{'raw text'} eq "true");
+# load command-line arguments: language pair, job numeric ID, filename of the submitted file
+my ($langPair, $jobId, $origFilename) = @ARGV;
 
-our $jobPathBase = $config->{'work dir'};
+# check if the language pair is set up in the configuration
+checkLangPair($config, $langPair);
 
-##### Translation #####
+# initialize filenames for intermediate files
+my $tmpFilenames = initTmpFilenames($config, $jobId);
 
-my ($langpair, $jobId, $filename) = @ARGV;
+# tokenize input using the configurated external script
+doTokenization($config, $tmpFilenames, $langPair);
 
-my ($srcLang, $tgtLang) = split(/-/, $langpair);
+##### translate and re-case input #####
 
-unless (defined($translHostList->{$langpair}) and defined($recaseHostList->{$langpair})) {
-	die("Failed to locate port for language pair `$langpair'");
-}
+# prepare proxies for translation and re-casing
+my $translProxy = getProxy($config, 'translation host list', $langPair);
+my $recaseProxy = getProxy($config, 'recasing host list', $langPair);
 
-### file name var init
-our $jobPath = $jobPathBase . "/" . $jobId;
-
-my ($inName, $tokName, $rawOutName, $outName, $tokLogName, $detokLogName, $timecodeName) = map { $jobPath . "/" . $_ }
-	qw(input.txt tok-input.txt raw-output.txt output.txt tok.log detok.log timecodes.dat);
-
-### tokenize and lower-case input
-
-# if in raw text mode, skip steps and send text instead of an XMLRPC request
-if ($rawTextMode) {
-	#skip tokenization
-	$tokName = $inName;
-}
-else {
-	my $tokScript = $config->{'tokenizer'};
-
-	system("$tokScript -l $srcLang <$inName >$tokName 2>$tokLogName");
-
-	unless ($? == 0 and -e $tokName) {
-		die("Failed to tokenize file ($!)");
-	}
-}
-
-### translate and re-case input
-
-my $translProxy = XMLRPC::Lite->proxy($translHostList->{$langpair});
-my $recaseProxy = XMLRPC::Lite->proxy($recaseHostList->{$langpair});
-
-my $inFh = fopen($tokName);
-my $outFh = fopen(">$rawOutName");
+# open input and output files
+my $inFh = fopen($tmpFilenames->{'tok'});
+my $outFh = fopen(">" . $tmpFilenames->{'rawtrans'});
 
 while (my $text = <$inFh>) {
 	# lower-case
-	my $lcText = ($rawTextMode? $text: lc($text));
+	my $lcText = lowerCase($config, $text);
 
 	# translate
-	my $rawOut = communicate($translProxy, $lcText);
+	my $rawOut = communicate($config, $translProxy, $lcText);
 	
 	# re-case
-	my $recasedOut = ($rawTextMode? $rawOut: finalizeRecasing(communicate($recaseProxy, $rawOut)));
+	my $recasedOut = reCase($config, $rawOut, $recaseProxy);
 	
 	# output the result
 	print $outFh $recasedOut . "\n";
@@ -75,33 +51,13 @@ while (my $text = <$inFh>) {
 close($outFh);
 close($inFh);
 
-### de-tokenize
+# de-tokenize output using the configurated external script
+doDeTokenization($config, $tmpFilenames, $langPair);
 
-my $detokScript = $config->{'detokenizer'};
+# report results by either performing a call-back (if the host is configured) or by updating the job the status
+reportResults($config, $jobId, $tmpFilenames, $origFilename);
 
-system("$detokScript -l $tgtLang <$rawOutName >$outName 2>$detokLogName");
-
-unless ($? == 0 and -e $tokName) {
-	die("Failed to de-tokenize file ($!)");
-}
-
-### report result
-
-my $dbPath = $config->{'db path'};
-
-our $dbh = DBI->connect("dbi:SQLite:dbname=$dbPath","","");
-
-# if the call-back URL is defined, perform call-back
-if (defined($config->{'call-back url'})) {
-	performCallBack($jobId, $outName, $filename);
-	
-	#delete the directory and update the status
-	cleanup($jobId, $jobPath);
-}
-# otherwise, save status for retrieval via getresults.php
-else {
-	saveStatus($jobId);
-}
+#####################################################
 
 #####
 #
@@ -119,9 +75,11 @@ sub fopen {
 #
 #####
 sub communicate {
-	my ($proxy, $text) = @_;
+	my ($config, $proxy, $text) = @_;
 	
 	$text =~ s/[\r\n]//g;
+	
+	my $rawTextMode = confBool($config->{'raw text mode'});
 	
 	if ($rawTextMode) {
 		#TODO ALS, please insert your communication code here
@@ -231,6 +189,15 @@ sub normalize {
 #####
 #
 #####
+sub confBool {
+	my ($str) = @_;
+	
+	return ($str eq "true");
+}
+
+#####
+#
+#####
 sub confHash {
 	my ($str) = @_;
 	
@@ -255,7 +222,7 @@ sub logHash {
 #
 #####
 sub saveStatus {
-	my ($jobId) = @_;
+	my ($dbh, $jobId) = @_;
 	
 	$dbh->do("update trids set is_done = 1 where id = $jobId");
 }
@@ -264,7 +231,7 @@ sub saveStatus {
 #
 #####
 sub cleanup {
-	my ($jobId) = @_;
+	my ($dbh, $jobId, $jobPath) = @_;
 	
 	$dbh->do("delete from trids where id = $jobId");
 	
@@ -275,14 +242,14 @@ sub cleanup {
 #
 #####
 sub performCallBack {
-	my ($jobId, $resultPath, $filename) = @_;
+	my ($jobId, $resultPath, $origFilename) = @_;
 	
 	my $curl = WWW::Curl::Easy->new;
 
 	my $curlForm = WWW::Curl::Form->new;
 	$curlForm->formaddfile($resultPath, "file", "multipart/form-data");
 	$curlForm->formadd("requestID", "" . $jobId);
-	$curlForm->formadd("fileName", $filename);
+	$curlForm->formadd("fileName", $origFilename);
 
 	$curl->setopt(CURLOPT_HTTPPOST, $curlForm);
 
@@ -298,5 +265,163 @@ sub performCallBack {
 	unless ($retcode == 0) {
 		die("An error happened at call-back: $retcode " .
 			$curl->strerror($retcode) . "; " . $curl->errbuf . "\n");
+	}
+}
+
+#####
+#
+#####
+sub checkLangPair {
+	my ($conf, $lp) = @_;
+	
+	my $translHostList = confHash($conf->{'translation host list'});
+	my $recaseHostList = confHash($conf->{'recasing host list'});
+	
+	unless (defined($translHostList->{$lp}) and defined($recaseHostList->{$lp})) {
+		die("Failed to locate port for language pair `$lp'");
+	}
+}
+
+#####
+#
+#####
+sub initTmpFilenames {
+	my ($config, $jobId) = @_;
+	
+	my $jobPath = $config->{'work dir'} . "/" . $jobId;
+	
+	# input = raw input file
+	# tok = tokenized input file
+	# rawtrans = translated, re-cased file
+	# detok = de-tokenized file
+	
+	# toklog = log file of the tokenizer
+	# detoklog = log file of the de-tokenizer
+	
+	# timecodes = timecode file
+	
+	return {map { $_ => $jobPath . "/" . $_ . ".txt" } qw(input tok rawtrans detok toklog detoklog timecodes)};
+}
+
+#####
+#
+#####
+sub doTokenization {
+	my ($config, $tmpNames, $langPair) = @_;
+	
+	my $rawTextMode = confBool($config->{'raw text mode'});
+	
+	# if in raw text mode, skip tokenization
+	if ($rawTextMode) {
+		$tmpNames->{'tok'} = $tmpNames->{'input'};
+	}
+	else {
+		my ($srcLang) = split(/-/, $langPair);
+		
+		my $tokScript = $config->{'tokenizer'};
+		
+		system("$tokScript -l $srcLang" .
+			" <" . $tmpNames->{'input'} .
+			" >" . $tmpNames->{'tok'} .
+			" 2>" . $tmpNames->{'toklog'});
+		
+		unless ($? == 0) {
+			die("Failed to tokenize file ($!)");
+		}
+	}
+	
+	unless (-e $tmpNames->{'tok'}) {
+		die("Tokenized input file was not generated");
+	}
+}
+
+#####
+#
+#####
+sub doDeTokenization {
+	my ($config, $tmpNames, $langPair) = @_;
+	
+	my $rawTextMode = confBool($config->{'raw text mode'});
+	
+	# if in raw text mode, skip de-tokenization
+	if ($rawTextMode) {
+		$tmpNames->{'detok'} = $tmpNames->{'rawtrans'};
+	}
+	else {
+		my ($srcLang, $tgtLang) = split(/-/, $langPair);
+		
+		my $detokScript = $config->{'detokenizer'};
+		
+		system("$detokScript -l $tgtLang" .
+			" <" . $tmpNames->{'rawtrans'} .
+			" >" . $tmpNames->{'detok'} .
+			" 2>" . $tmpNames->{'detoklog'} );
+		
+		unless ($? == 0) {
+			die("Failed to de-tokenize file ($!)");
+		}
+	}
+	
+	unless (-e $tmpNames->{'detok'}) {
+		die("De-tokenized output file was not generated");
+	}
+}
+
+#####
+#
+#####
+sub getProxy {
+	my ($config, $listId, $lp) = @_;
+	
+	my $list = confHash($config->{$listId});
+	
+	return XMLRPC::Lite->proxy($list->{$lp});
+}
+
+#####
+#
+#####
+sub lowerCase {
+	my ($config, $text) = @_;
+	
+	my $rawTextMode = confBool($config->{'raw text mode'});
+	
+	return ($rawTextMode? $text: lc($text));
+}
+
+#####
+#
+#####
+sub reCase {
+	my ($config, $rawTranslation, $proxy) = @_;
+	
+	my $rawTextMode = confBool($config->{'raw text mode'});
+	
+	return ($rawTextMode? $rawTranslation: finalizeRecasing(communicate($config, $proxy, $rawTranslation)));
+}
+
+#####
+#
+#####
+sub reportResults {
+	my ($config, $jobId, $tmpNames, $origFilename) = @_;
+	
+	my $jobPath = $config->{'work dir'} . "/" . $jobId;
+	
+	# initialize a DB connection
+	my $dbPath = $config->{'db path'};
+	my $dbh = DBI->connect("dbi:SQLite:dbname=$dbPath","","");
+	
+	# if the call-back URL is defined, perform call-back
+	if (defined($config->{'call-back url'})) {
+		performCallBack($jobId, $tmpNames->{'detok'}, $origFilename);
+		
+		#delete the directory and update the status
+		cleanup($dbh, $jobId, $jobPath);
+	}
+	
+	# otherwise, save status for retrieval via getresults.php
+	else {
+		saveStatus($dbh, $jobId);
 	}
 }
