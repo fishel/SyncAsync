1645ca17206fdd1d9c9d45671ca235f0f6b6aaab
Mark Fishel <mphi@kitt2>
2013-09-24 15:35:36 +0200

    sdggs

--- offweb-files/translate_file.pl
+++ offweb-files/translate_file.pl
@@ -699,7 +699,7 @@ sub generateSrtFile {
 	
 	my $convScript = $Bin . "/txt2srt.pl";
 	
-	my $cmd = sprintf("%s < %s | iconv -c -f utf8 -t latin1 > %s", $convScript, $tmpFilenames->{'output'}, $tmpFilenames->{'srt'});
+	my $cmd = sprintf("%s < %s | iconv -c -f utf8 -t cp1252 > %s", $convScript, $tmpFilenames->{'output'}, $tmpFilenames->{'srt'});
 	
 	system($cmd);
 }
